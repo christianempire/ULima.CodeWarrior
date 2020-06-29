72a6b408be62c47c5e5a7ff5ad9ddb88abc171cc
Christian <christianempire@hotmail.com>
2020-06-29 09:49:18 -0500

    Finished level005b

---
+++ Assets/_Levels/Level005/Level005B.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Level005;
+using System;
+
+namespace Assets._Levels.Level005
+{
+    public class Level005B : Level005Script
+    {
+        public override void Main()
+        {
+            // Defeat the enemies.
+            // Remember that they each take two hits.
+            Hero.Attack("Gurt");
+            Hero.Attack("Gurt");
+            Hero.Attack("Rig");
+            Hero.Attack("Rig");
+            Hero.Attack("Ack");
+            Hero.Attack("Ack");
+        }
+    }
+}
